33b7f339a87b4bfda10ebb95e2e1c94dd3f3494f
WesleySerrano <wesleyfaserrano@hotmail.com>
2015-10-01 19:14:06 -0300

    - Gráfico de linha inserido

--- interface.js
+++ interface.js
@@ -18,3 +18,38 @@ function onDistributionValueChanged()
         document.getElementById("uniformValues").innerHTML = "";
     }
 }
+
+function chart()
+{
+    var lineChartData = {
+        labels : ["January","February","March","April","May","June","July","August"],
+        datasets : [
+            {
+                label: "My First dataset",
+                fillColor : "rgba(220,220,220,0.2)",
+                strokeColor : "rgba(220,220,220,1)",
+                pointColor : "rgba(220,220,220,1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(220,220,220,1)",
+                data : [1,2,3,4,5,6,7,8]
+            },
+            {
+                label: "My Second dataset",
+                fillColor : "rgba(151,187,205,0.2)",
+                strokeColor : "rgba(151,187,205,1)",
+                pointColor : "rgba(151,187,205,1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(151,187,205,1)",
+                data : [8,7,6,5,4,3,2,1]
+            }
+        ]
+
+    }
+
+    var ctx = document.getElementById("canvas").getContext("2d");
+    window.myLine = new Chart(ctx).Line(lineChartData, {
+        responsive: true
+    });
+}
